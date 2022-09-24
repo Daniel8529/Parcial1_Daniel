8b8581b6ee8e1c184ef23fe33192456b68555e09
Daniel8529 <97201981+Daniel8529@users.noreply.github.com>
2022-09-24 00:38:51 -0400

    Colocando la navegacion en Main Activity

--- app/src/main/java/com/ucne/parcial1_daniel/MainActivity.kt
+++ app/src/main/java/com/ucne/parcial1_daniel/MainActivity.kt
@@ -4,7 +4,24 @@ import android.os.Bundle
 
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.ucne.parcial1_daniel.navegation.Screen
+import com.ucne.parcial1_daniel.ui.List.ListScreen
+import com.ucne.parcial1_daniel.ui.Registro.GenericoScreen
+import com.ucne.parcial1_daniel.ui.theme.Parcial1_DanielTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,9 +29,24 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Parcial1_DanielTheme {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController, startDestination = Screen.ListScreen.route
+                ) {
 
+                    composable(Screen.ListScreen.route) {
 
+                        ListScreen(
+                            onClick = { navController.navigate(Screen.GenericoScreen.route) }
+                        )
+                    }
+                    composable(Screen.GenericoScreen.route) {
+                        GenericoScreen({ navController.navigateUp() })
+                    }
 
+                }
+            }
 
 
 
