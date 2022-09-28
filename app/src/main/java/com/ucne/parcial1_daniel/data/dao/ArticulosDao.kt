0ec0b91747a5dcd14510c1839466015a9896b1c9
Daniel8529 <97201981+Daniel8529@users.noreply.github.com>
2022-09-28 18:35:05 -0400

    creando el dao

--- app/src/main/java/com/ucne/parcial1_daniel/data/dao/ArticulosDao.kt
+++ app/src/main/java/com/ucne/parcial1_daniel/data/dao/ArticulosDao.kt
@@ -10,14 +10,14 @@ import androidx.room.*
 import com.ucne.parcial1_daniel.data.entity.Articulos
 
 @Dao
-interface Articulodao {
+interface ArticulosDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(articulos: Articulos)
 
     @Update()
     suspend fun update(articulos: Articulos)
 
-    @Query("SELECT *"+"FROM Articulos" + " WHERE ArticuloId= :id ")
+    @Query("SELECT * FROM Articulos  WHERE ArticuloId= :id ")
     fun get(id : Int): Flow<Articulos>
 
     @Query("SELECT * FROM Articulos")
