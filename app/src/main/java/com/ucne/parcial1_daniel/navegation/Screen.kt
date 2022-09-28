a099c6ed1dde14baa86e9e209c26ddbb202c6343
Daniel8529 <97201981+Daniel8529@users.noreply.github.com>
2022-09-28 19:33:01 -0400

    Colocando el oject en screen

--- app/src/main/java/com/ucne/parcial1_daniel/navegation/Screen.kt
+++ app/src/main/java/com/ucne/parcial1_daniel/navegation/Screen.kt
@@ -3,8 +3,8 @@ package com.ucne.parcial1_daniel.navegation
 sealed class Screen(val route: String){
     object ListScreen: Screen(" ListScreen")
     object ListViewModel: Screen("ListViewModel")
-    object GenericoScreen: Screen("  GenericoScreen")
-    object GenericoViewModel: Screen("GenericoViewModel")
+    object ArticulosScreen: Screen("   ArticulosScreen")
+    object ArticulosViewModel: Screen(" ArticulosScreen")
 
 
 }
